4eaf79ee229a1ccb1a17d0ab6c86a52d36d3fda3
Ultraleet <ultraleet@gmail.com>
2017-01-24 11:54:53 +0200

    Setup gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass", function () {
     return gulp.src("src/scss/*.scss")
         .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['> .1%'],
+            cascade: false
+        }))
         .pipe(rename("jquery.ultraselect.css"))
         .pipe(gulp.dest("dist"));
 });
